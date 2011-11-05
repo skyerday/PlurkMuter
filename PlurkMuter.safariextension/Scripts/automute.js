8128f360a7487005609c39523714cc199c338200
Skyer Tai <skyer.tw@gmail.com>
2011-11-05 23:58:04 +0800

    Use embedded UI in Plurk to do setting. Remove tool item in Safari.

--- PlurkMuter.safariextension/Scripts/automute.js
+++ PlurkMuter.safariextension/Scripts/automute.js
@@ -11,7 +11,7 @@
 // ver 0.3 (2011/9/20)  * Support account name for the keyword. Ex: http://www.plurk.com/off60, you can assign off60 for blockList item.
 // ver 0.4 (2011/11/01) * Change to Safari extension format. Remove blockList. It will be added outside.
 // ver 0.5 (2011/11/04) * Fix error in FF && Chrome
-// ver 0.6 (2011/11/xx) * Fix error in Safari
+// ver 0.6 (2011/11/05) * Add UI in Plurk. Click Muter in Plurk title to show setting.
 // ==/UserScript==
 
 (function(){
@@ -24,33 +24,108 @@ var myWindow = (typeof unsafeWindow == 'undefined') ? window : unsafeWindow;
 
 function waitJQ() {
     if(typeof myWindow.jQuery == 'undefined') {
-        window.setTimeout(waitJQ,100);
+        myWindow.setTimeout(waitJQ,100);
     } else {
-        doRTE(myWindow.jQuery.noConflict(true));
+        myWindow.jq = myWindow.jQuery.noConflict(true);
+        myWindow.signread = false;
+        myWindow.mutesetting = undefined;
+        injectMuter(myWindow.jq);
     }
 }
 
 waitJQ();
 
+if (typeof GM_getValue == 'undefined') {
+    GM_getValue = function(key) {
+        console.log('getvalue: ' + window.gmgetsetting);
+        return window.gmgetsetting;  
+    };
+}
+
+if (typeof GM_setValue == 'undefined') {
+    GM_setValue = function(key, value) {
+        myWindow.keykeykey = value;
+        var v = document.getElementById('proxy_for_data');
+        v.innerText = value;
+    };
+}
+
+function fetchKeywords(keyword, ui) {
+    if (typeof keyword == 'undefined' || keyword.length <= 0) return;
+    var arr = keyword.split(",");
+    var list = document.getElementById("keywords");
+    outSideBlockList = [];
+    for (var i=0; i<arr.length; ++i) {
+        if (arr[i].length > 0) {
+            if (ui) {
+                var option = document.createElement("option");
+                option.text = arr[i];
+                list.add(option, null);
+            }
+            outSideBlockList.push(arr[i]);
+        }
+    }
+}
+
+function injectMuter($) {
+    $('#top_bar td').append('<a id="plurkmuter" href="#" class="item">Muter</a>');
+    $('#plurkmuter').click(function(){
+        myWindow.jq('#mutersetting').css('display', '');
+    });
+    $('body').append(
+        '<script type="text/javascript">function closeSetting(){window.jq(\'#mutersetting\').css({\'display\':\'none\'});}' +
+        '   function updateSetting() {var list = document.getElementById("keywords"); var setting = ""; for (var i=0; i<list.length; ++i) { setting = setting + "," + list.options[i].text; } if (setting.length > 0) setting = setting.substring(1, setting.length); window.mutersetting = setting; window.signread = true;}' +
+        '   function onAddClick() {var list = document.getElementById("keywords"); var input = document.getElementById("keyword"); var option = document.createElement("option"); option.text = input.value; list.add(option, null); updateSetting();}' +
+        '   function onDelClick() {var list = document.getElementById("keywords"); var index = list.selectedIndex; if (index == -1) return; list.remove(index); updateSetting();} ' +
+        '</script>' +
+        '<div id="mutersetting" style="color: #000000; position:absolute; top:100px; left:100px; background-color:#FFFFFF; width: 500px; height: 300px; z-index: 5000; overflow: auto; display: none;">' +
+        '<p>Auto mute plurk keywords</p>' +
+        '<form>' +
+        'Keyword: <input name="keyword" id="keyword" /> <button name="add" id="add" type="button" onClick="onAddClick();">Add</button>' +
+        '<button name="delkey" id="delkeys" type="button" onClick="onDelClick();">Delete</button><br>' +
+        '<select name="keywords" id="keywords" multiple="multiple" size="10" width="300" style="width: 300px;"></select> <br>' +
+        '<p>&nbsp;&nbsp;&nbsp;&nbsp;by Skyer 2011</p>' +
+        '<button name="close" id="close" type="button" onClick="closeSetting()">Close</button>' +
+        '</form>' +
+        '</div>');
+    console.log('Get Setting: ' + GM_getValue('mutersetting'));
+    fetchKeywords(GM_getValue('mutersetting'), true);
+    window.setTimeout(doRTE, 100, $);
+}
+
 function doRTE($) {
+    if (myWindow.signread == true) {
+        var setting = myWindow.mutersetting;
+        myWindow.signread = false;
+        console.log('MuterSetting: ' + setting);
+        GM_setValue("mutersetting", setting);
+        fetchKeywords(setting, false);
+    }
+
     var blockList = typeof outSideBlockList == 'undefined' ? undefined : outSideBlockList;
+    if (typeof blockList == 'undefined') {
+        myWindow.setTimeout(doRTE, 2000, $);
+        return;
+    }
+
 
         // No need to set blockList here. It will be set outside.
         //Add custom keywords and user id here!
         //在下面依照格式加入你想過濾的關鍵字或者帳號
-        if (blockList == undefined) {
-            blockList = [
-                'TW_nextmedia', //蘋果日報
-                'cwyuni',       //酪梨壽司
-                'off60',        //四小折 
-                'FuniPhone',    //Fun iPhone
-                'wan_wan',      //彎彎~
-                'alvin',        //alvin
-                'Jojam',        //就醬
-                'giddens',      //一直說不客氣不客氣的九把刀
-                'jcms',         //吉米丘
-            ];
-        }
+        //console.log('blocklist: ' + blockList);
+        // if (blockList == undefined) {
+        //     blockList = [
+        //         'TW_nextmedia', //蘋果日報
+        //         'cwyuni',       //酪梨壽司
+        //         'off60',        //四小折 
+        //         'FuniPhone',    //Fun iPhone
+        //         'wan_wan',      //彎彎~
+        //         'alvin',        //alvin
+        //         'Jojam',        //就醬
+        //         'giddens',      //一直說不客氣不客氣的九把刀
+        //         'jcms',         //吉米丘
+        //     ];
+        // }
 
         function do_match(text) {
             for (k=0;k<=blockList.length;k++) {
--- PlurkMuter.safariextension/Scripts/inject.js
+++ PlurkMuter.safariextension/Scripts/inject.js
@@ -3,7 +3,7 @@ var blockList = "var outSideBlockList = [";
 function loop() {
 	var blockScript = document.createElement('script');
 	blockScript.type = 'text/javascript';
-	blockScript.text = blockList;
+	blockScript.text = 'window.gmgetsetting=\'' + window.gmgetsetting + '\';';
 	document.body.appendChild(blockScript);
 
 	var script = document.createElement('script');
@@ -18,18 +18,23 @@ function performMessage(event) {
 	if (event.name == "mutekeyword") {
 		if (event.message.length == 0) return;
 		
-		var arr = event.message.split(",");
-		for (var i=0; i<arr.length; ++i) {
-			if (arr[i].length > 0) {
-				blockList = blockList + "'" + arr[i] + "',"
-			}
-		}
-		if (blockList.charAt(blockList.length-1) === ",") blockList = blockList.substring(0, blockList.length-1);
-		blockList = blockList + "];";
-		console.log(blockList);
-
+		window.gmgetsetting = event.message;
+		console.log('mute: ' + window.gmgetsetting);
+		
 		window.setTimeout(loop, 2000);
 	}
+}
+
+function dataMonitor() {
+	var v = document.getElementById('proxy_for_data');
+	if (v) {
+		if (v.innerText && v.innerText.length > 0) {
+			var text = v.innerText;
+			v.innerText = '';
+			safari.self.tab.dispatchMessage('update', text);
+		}
+	}
+	window.setTimeout(dataMonitor, 1000);
 }
 
 if (window.top === window) {
@@ -37,4 +42,11 @@ if (window.top === window) {
 
 	safari.self.addEventListener("message", performMessage, false);
 	safari.self.tab.dispatchMessage("fetch", 1);
+
+	var span = document.createElement('span');
+	span.id = 'proxy_for_data';
+	span.style.display = 'none';
+	document.body.appendChild(span);
+
+	window.setTimeout(dataMonitor, 1000);
 }
